70b18b9aba13faece188130a9eb6e61c1bd7fe63
henry2one <enrique.rs.luis@gmail.com>
2023-06-27 11:43:39 -0600

    Added app to the repository

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import {Router} from 'express'
+
+const router = Router()
+
+router.get('/', (req, res) => res.render('index',{ title: 'Website with Node', x: 'Henry2One'}))
+router.get('/about', (req, res) => res.render('about',{ title: 'About me' }))
+router.get('/contact', (req, res) => res.render('contact',{ title: 'Contact Page' }))
+
+export default router 
